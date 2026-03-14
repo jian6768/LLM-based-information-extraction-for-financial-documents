6fd93db23ed053b1bed630637d3ef1c95e66a5e8
KJ <jian6768@gmail.com>
2026-03-15 03:37:04 +0800

    addition of non LLM extraction

--- document_parser_langchain.py
+++ document_parser_langchain.py
@@ -4,6 +4,7 @@ import os
 from docling.document_converter import DocumentConverter
 from pathlib import Path
 import pandas as pd
+from langchain_docling import DoclingLoader
 
 class DocumentParser():
 
@@ -13,8 +14,9 @@ class DocumentParser():
         use_docling_loader: bool = True,
     ) -> None:
         
-        self.use_docling_loader = use_docling_loader
         self.out_dir = out_dir or os.getcwd()
+        self.use_docling_loader = use_docling_loader
+        
 
     def ingest_document(self, file_path: str):
 
@@ -23,12 +25,19 @@ class DocumentParser():
         stem = path.stem
         extension = path.suffix
 
-        if path.suffix == ".pdf":
+        if extension == ".pdf":
 
             print(f"Processing file: {filename}")
 
             self.ticker = stem.split("_")[0]
             self.FP = stem.split("_")[-2]
+
+
+            #Instantiate doclingloader
+            loader = DoclingLoader(path)
+
+            #loads documents
+            docs = loader.load()
 
             converter = DocumentConverter()
             #self.result is of document result class. Within self.result class includes a Document class that has attributes like texts, tables and methods like export to markdown. 
@@ -36,24 +45,24 @@ class DocumentParser():
             self.doc = self.result.document
             
             #export document as markdown
-            markdown_folder_path = Path(os.path.join(self.out_dir,"markdown"))
-            markdown_folder_path.mkdir(parents = True, exist_ok= True)
             self.doc.save_as_markdown(os.path.join(self.out_dir,"markdown",stem + ".md"))
 
-            #export document as json
-            json_folder_path = Path(os.path.join(self.out_dir,"json"))
-            json_folder_path.mkdir(parents=True,exist_ok=True)
-            self.doc.save_as_json(os.path.join(self.out_dir,"json",stem + ".json"))
-            
 
             #Create table folder if it does not exist
-            tables_folder_path = Path(os.path.join(self.out_dir,"tables"))
+            # tables_folder_path = Path(os.path.join(self.out_dir,"tables"))
+            tables_folder_path = Path(os.path.join(self.out_dir,"tables",self.ticker,self.FP))
             tables_folder_path.mkdir(parents = True, exist_ok= True)
 
             for idx, table in enumerate(self.doc.tables):
                 #docling now requires export to dataframe to take in doc. 
                 df = table.export_to_dataframe(doc=self.doc)
-                df.to_csv(tables_folder_path/(stem + f"-table-{idx}"))
+                df.to_csv(tables_folder_path/(stem + f"-table-{idx}.csv"))
+
+             #export document as json
+            json_folder_path = Path(os.path.join(self.out_dir,"json"))
+            json_folder_path.mkdir(parents=True,exist_ok=True)
+            self.doc.save_as_json(os.path.join(self.out_dir,"json",stem + ".json"))
+
 
         else:
             print("Invalid document format")
